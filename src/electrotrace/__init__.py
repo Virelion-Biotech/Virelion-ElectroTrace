4db672f0db5a07bc5f0656ce863f47e2e8fd3b32
Jason Ramirez <syedumerhannan@proton.me>
2026-08-19 03:05:07 +0300

    Expose research provenance helpers

--- src/electrotrace/__init__.py
+++ src/electrotrace/__init__.py
@@ -1,3 +1,15 @@
 """ElectroTrace: research-grade ECG and electrophysiology software."""
 
 __version__ = "1.6.1"
+
+from .provenance import DatasetManifest, manifest_from_dict
+from .research_validation import build_validation_report, summarize_records_rigorous, write_validation_report
+
+__all__ = [
+    "__version__",
+    "DatasetManifest",
+    "manifest_from_dict",
+    "build_validation_report",
+    "summarize_records_rigorous",
+    "write_validation_report",
+]
