3924e1b83f820e9a350d296cfc5e37475d7b34b0
Jason Ramirez <syedumerhannan@proton.me>
2026-08-19 03:05:32 +0300

    Synchronize runtime version to 1.7.0

--- src/electrotrace/__init__.py
+++ src/electrotrace/__init__.py
@@ -1,6 +1,6 @@
 """ElectroTrace: research-grade ECG and electrophysiology software."""
 
-__version__ = "1.6.1"
+__version__ = "1.7.0"
 
 from .provenance import DatasetManifest, manifest_from_dict
 from .research_validation import build_validation_report, summarize_records_rigorous, write_validation_report
