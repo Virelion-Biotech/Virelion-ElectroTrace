0193072fb3757c4661b2d3ac75cb118421c2a59c
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-09-20 16:48:40 +0300

    test: cover detector registry aliases

---
+++ tests/test_detector_registry.py
@@ -0,0 +1,11 @@
+from electrotrace.detectors import get_detector
+
+
+def test_builtin_detector_registry_contains_classical_detectors():
+    assert get_detector("pan-tompkins").name == "pan-tompkins"
+    assert get_detector("hamilton").name == "hamilton"
+    assert get_detector("stage1").name == "stage1"
+
+
+def test_detector_alias_is_normalized():
+    assert get_detector("pan_tompkins").name == "pan-tompkins"
