e7d6cdb9b19c7db51b317a5b97c0fb957f1ca585
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-09-20 17:03:11 +0300

    test: cover optional WFDB gqrs detector registry entry

--- tests/test_detector_registry.py
+++ tests/test_detector_registry.py
@@ -5,6 +5,7 @@ def test_builtin_detector_registry_contains_classical_detectors():
     assert get_detector("pan-tompkins").name == "pan-tompkins"
     assert get_detector("hamilton").name == "hamilton"
     assert get_detector("stage1").name == "stage1"
+    assert get_detector("gqrs").name == "wfdb-gqrs"
 
 
 def test_detector_alias_is_normalized():
